354e461458fd7c9da821f3c3540101db549cc7e0
jasurn <97jasur@gmail.com>
2020-04-23 05:53:25 +0500

    Add production and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,6 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "main.[contentHash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/template.html"
     })],
