84fbad4af27938f7fa6061c3634ac29cc06be16e
jasurn <97jasur@gmail.com>
2020-04-23 07:32:38 +0500

    Add extract css
    -minify html/css / js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
@@ -8,4 +9,15 @@ module.exports = merge(common, {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ["style-loader",
+                "css-loader",
+                "sass-loader"]
+        },]
+    }
 });
